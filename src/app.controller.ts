1b36c6a961548772e05c7d4b6e784f9aa572fe9c
wideyedwonderer <codisanko@gmail.com>
2021-01-24 12:17:37 +0200

    incorporated logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,46 +3,70 @@ import { ConfigService } from '@nestjs/config';
 import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import * as nodemailer from 'nodemailer';
+import {MindfulLogger, MindfulMsPayload, SendEmailOpts} from 'mindful-commons';
 
 @Controller()
 export class AppController {
   private host: string;
   private port: string;
   private secure: boolean;
-  private username: string;
+  private username: string; 
   private password: string;
 
   private  usernamePasswordMap = new Map<string, string>();
   constructor(
     private readonly appService: AppService,
-    private readonly configService: ConfigService) {
+    private readonly configService: ConfigService,
+    private readonly logger: MindfulLogger 
+    ) {
       this.host = this.configService.get('email.host');
       this.port = this.configService.get('email.port');
       this.secure = this.configService.get('email.secure');
 
+      if(!this.host) {
+        this.logger.error('Not found config for email.host', 'Out of context')
+        throw new Error('Not found config for email.host')
+      }
+      if(!this.port) {
+        this.logger.error('Not found config for email.port', 'Out of context')
+        throw new Error('Not found config for email.port')
+      }
+      if(!this.secure) {
+        this.logger.error('Not found config for email.secure', 'Out of context')
+        throw new Error('Not found config for email.secure')
+      }
       const usernames = this.configService.get('email.usernames').split(",");
       const passwords = this.configService.get('email.passwords').split(",");
-      
+      if(usernames.length ===0) {
+        this.logger.error('Not found config for email.usernames', 'Out of context')
+        throw new Error('Not found config for email.usernames')
+      }
+      if(passwords.length !== usernames.length) {
+        this.logger.error('Wrong config for email.passwords', 'Out of context')
+        throw new Error('Wrong config for email.passwords')
+      }
       usernames.forEach((username, index) => {
         this.usernamePasswordMap.set(username, passwords[index]);
       })
     }
   
     @EventPattern("send-email")    
-    async handleSendEmail(data: SendEmailOpts) {  
-      const username = data.from.split('<')[1].split('>')[0];
-     const password = this.usernamePasswordMap.get(username);
-     if(!password) {   
-        throw new Error('Password for this username not found')
-     }
+    async handleSendEmail(mindfulPayload: MindfulMsPayload<SendEmailOpts>) { 
 
-     if(!data.to) {
+      const loggedUserEmailIfProvided = mindfulPayload.loggedUser ? mindfulPayload.loggedUser.email : null;
+      const username = mindfulPayload.payload.from.split('<')[1].split('>')[0];
+     const password = this.usernamePasswordMap.get(username);
+
+     this.logger.log('About to send email to recepient', mindfulPayload.trace,loggedUserEmailIfProvided)
+     if(!mindfulPayload.payload.to) {
+      this.logger.error(`No email recepient`, mindfulPayload.trace, loggedUserEmailIfProvided)
         throw new Error('No Email Recipient')
       }
 
+      try { 
         let transporter = nodemailer.createTransport({
             
-          host: this.host,
+            host: this.host,
             port: this.port,
             secure: this.secure,
             auth: {
@@ -53,7 +77,7 @@ export class AppController {
             
         });
 
-         await transporter.sendMail(data, async (error, info) => {
+         await transporter.sendMail(mindfulPayload.payload, async (error, info) => {
                 if (error) {      
                   console.log('Could not send email, err', error);
                   }                
@@ -62,19 +86,13 @@ export class AppController {
     
           
             );      
-  }
-}
-export interface SendEmailOpts {
-  from: string,
-  to: string,
-  subject: string,
-  text: string,
-  html: string,
-  attachments?: EmailFileAttachment[];
+  
+      } catch(e) {
+          this.logger.error('Something went wrong when sending email' + e.toString(), loggedUserEmailIfProvided)
+      }
+
+    }
 }
 
-export interface EmailFileAttachment {
-  filename: string,
-  content: Buffer,
-  contentType: string;
-}
+
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './configuration';
+import {LoggerModule} from 'mindful-commons';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true, load: [configuration]}),
+    LoggerModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+   
+  ],
 })
 export class AppModule {}
