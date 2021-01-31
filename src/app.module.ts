5afd27d5159ecb573df551c5b9233bed12cce660
wideyedwonderer <codisanko@gmail.com>
2021-01-31 13:24:50 +0200

    swap Logger for Core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
-import {LoggerModule} from 'mindful-commons';
+import {MindfulCoreModule} from 'mindful-commons';
 
+@Global()
 @Module({
   imports: [
+    MindfulCoreModule,
     ConfigModule.forRoot({isGlobal: true, load: [configuration]}),
-    LoggerModule
   ],
   controllers: [AppController],
   providers: [
     AppService,
    
   ],
+  exports: [MindfulCoreModule]
 })
 export class AppModule {}
